fbe6d2d79002b7777f4a454128cb20edc70b8e74
ClemenceVan <72015909+ClemenceVan@users.noreply.github.com>
2022-12-23 00:24:56 +0100

    push trés enervé : MHMMM DOC T'ES TERMINE J'AI ADD LES MSG RANDOM

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pytz
 import time
 import discord
 import asyncio
+import random
 
 def get_start_tmstp():
     tz = pytz.timezone('Europe/Paris')
@@ -27,30 +28,37 @@ def get_daily_play_time():
         url = f"https://europe.api.riotgames.com/lol/match/v5/matches/{match}?api_key={API_KEY}"
         match_data = requests.get(url)
         match_duration_total += match_data.json()['info']['gameDuration']
+    return 3.1, len(historic) #debug
     return match_duration_total / 3600, len(historic)
 
 class MyClient(discord.Client):
     async def my_loop(self):
-        play_time, match_played = get_daily_play_time()
-        embedVar = discord.Embed(title="Doc a encore fraudé", color=0xFF0000)
-        embedVar.add_field(name="Durée du préjudice ?", value=f"{timedelta(hours=play_time)}", inline=False)
-        embedVar.add_field(name="Piece a conviction", value="https://www.op.gg/summoners/euw/Larry444")
-        embedVar.set_footer(text="Made by 0xCurtis & Coach")
-        if play_time == 0:
-            await self.change_presence(activity=discord.Game(name=f"Doc n'a pas (encore) joué aujourd'hui. Bon toutou"))
-        else:
-            await self.change_presence(activity=discord.Game(name=f"Doc a joué {timedelta(hours=play_time).rsplit(':', 1)[0]} aujourd'hui"))
-
-        if play_time > 3 and self.triggered == False:
-            await self.humiliation_channel.send('hello, doc t fini')
-            self.trigger == True
-        elif play_time > 3 and self.triggered == True and match_played > self.match_played:
-            await self.humiliation_channel.send(embed=embedVar)
-        self.match_played = match_played
-        time.sleep(30)
+        debug = 0
+        while True:
+            play_time, match_played = get_daily_play_time()
+            debug = debug + 1
+            match_played = debug #debug
+            print(play_time)
+            print(match_played)
+            embedVar = discord.Embed(title="Doc a encore fraudé", color=0xFF0000)
+            embedVar.add_field(name="Durée du préjudice ?", value=f"{timedelta(hours=play_time)}", inline=False)
+            embedVar.add_field(name="Piece a conviction", value="https://www.op.gg/summoners/euw/Larry444")
+            embedVar.set_footer(text="Made by 0xCurtis & Coach")
+            if play_time == 0:
+                await self.change_presence(activity=discord.Game(name=f"Doc n'a pas (encore) joué aujourd'hui. Bon toutou"))
+            else:
+                await self.change_presence(activity=discord.Game(name=f"Doc a joué {str(timedelta(hours=play_time)).rsplit(':', 1)[0]} aujourd'hui"))
+            if play_time > 3 and self.triggered == False:
+                await self.humiliation_channel.send(embed=embedVar)
+                self.triggered = True
+            elif play_time > 3 and self.triggered == True and match_played > self.match_played:
+                await self.humiliation_channel.send(str(get_random_messages(play_time)))
+            self.match_played = match_played
+            time.sleep(1) #must be 30
 
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+        self.triggered = False
         self.humiliation_channel = client.get_guild(908448072730837139).get_channel(1055585865541353564)
         self.match_played = 0
         await self.my_loop()
@@ -63,6 +71,17 @@ class MyClient(discord.Client):
 API_KEY = "RGAPI-33ccdc25-6a4a-4e06-a61e-3002ce16ad96"
 SPOT_PUUID = "64c8Bnqtzo4JZX7OMpHKOCVoxlTi9KSwNEBxj9c710XnLDVPgYG4tw8ddb5AaEW_q9xOOgckB0Bjfg"
 DISCORD_TOKEN = "MTA1NTU4NTA3ODc0ODY2MzgxOA.GbBDYa.dV9TqC_Ro4YBnHOo2wPBeGGTdDJ_ToetsVYDVM"
+
+def get_random_messages(play_time):
+    time = str(timedelta(hours=play_time)).rsplit(':', 1)[0]
+    array = [
+        f":pensive: {time}",
+        f"Il continue ... il joue depuis maintenant {time} heures",
+        f"https://tenor.com/view/fortnite-aura-take-the-l-dancing-loss-gif-21664886 {time} heures",
+        f"Il aurait pu passer {time} heures a se contruire un futur mais il a préféré relancé une game de lol",
+        f"Doc est bloqué sur la faille depuis {time} heures",
+    ]
+    return array[random.randint(0, len(array) - 1)]
 
 intents = discord.Intents(value=8, guilds=True)
 client = MyClient(intents=intents)
