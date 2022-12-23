3018637f5f1c5d789af934f20d82559823b4fe94
ClemenceVan <72015909+ClemenceVan@users.noreply.github.com>
2022-12-23 01:25:24 +0100

    push tres cool : mhm doc finito j'ai clean le code

--- main.py
+++ main.py
@@ -4,73 +4,69 @@ from datetime import datetime
 import pytz
 import time
 import discord
-import asyncio
+from discord.ext import tasks
 import random
 
-def get_start_tmstp():
-    tz = pytz.timezone('Europe/Paris')
-    return int(datetime.now(tz=tz).replace(hour=0, minute=0, second=0, microsecond=0).timestamp())
+class lolSniper():
+    def __init__(self):
+        self.API_KEY = "RGAPI-33ccdc25-6a4a-4e06-a61e-3002ce16ad96"
+        self.SPOT_PUUID = "64c8Bnqtzo4JZX7OMpHKOCVoxlTi9KSwNEBxj9c710XnLDVPgYG4tw8ddb5AaEW_q9xOOgckB0Bjfg"
+        self.base_url = "https://europe.api.riotgames.com/lol/match/v5/matches/"
 
-def get_day_history(day_timestamp, retries=2):
-    if retries > 0:
-        BASE_URL = f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{SPOT_PUUID}/ids?startTime={day_timestamp}&start=0&count=50&api_key={API_KEY}"
-        res = requests.get(BASE_URL)
-        if res.status_code == 200:
-            return res.json()
-        else:
-            time.sleep(120)
-            return get_day_history(day_timestamp, retries - 1)
-
-def get_daily_play_time():
-    historic = get_day_history(get_start_tmstp())
-    match_duration_total = 0
-    for match in historic:
-        url = f"https://europe.api.riotgames.com/lol/match/v5/matches/{match}?api_key={API_KEY}"
-        match_data = requests.get(url)
-        match_duration_total += match_data.json()['info']['gameDuration']
-    return 3.1, len(historic) #debug
-    return match_duration_total / 3600, len(historic)
-
-class MyClient(discord.Client):
-    async def my_loop(self):
-        debug = 0
-        while True:
-            play_time, match_played = get_daily_play_time()
-            debug = debug + 1
-            match_played = debug #debug
-            print(play_time)
-            print(match_played)
-            embedVar = discord.Embed(title="Doc a encore fraudé", color=0xFF0000)
-            embedVar.add_field(name="Durée du préjudice ?", value=f"{timedelta(hours=play_time)}", inline=False)
-            embedVar.add_field(name="Piece a conviction", value="https://www.op.gg/summoners/euw/Larry444")
-            embedVar.set_footer(text="Made by 0xCurtis & Coach")
-            if play_time == 0:
-                await self.change_presence(activity=discord.Game(name=f"Doc n'a pas (encore) joué aujourd'hui. Bon toutou"))
+    def get_day_history(self, day_timestamp, retries=2):
+        if retries > 0:
+            url = f"{self.base_url}by-puuid/{self.SPOT_PUUID}/ids?startTime={day_timestamp}&start=0&count=50&api_key={self.API_KEY}"
+            res = requests.get(url)
+            if res.status_code == 200:
+                return res.json()
             else:
-                await self.change_presence(activity=discord.Game(name=f"Doc a joué {str(timedelta(hours=play_time)).rsplit(':', 1)[0]} aujourd'hui"))
-            if play_time > 3 and self.triggered == False:
-                await self.humiliation_channel.send(embed=embedVar)
-                self.triggered = True
-            elif play_time > 3 and self.triggered == True and match_played > self.match_played:
-                await self.humiliation_channel.send(str(get_random_messages(play_time)))
-            self.match_played = match_played
-            time.sleep(1) #must be 30
+                time.sleep(120)
+                return self.get_day_history(day_timestamp, retries - 1)
+
+    def get_daily_play_time(self):
+        tz = pytz.timezone('Europe/Paris')
+        timestamp = int(datetime.now(tz=tz).replace(hour=0, minute=0, second=0, microsecond=0).timestamp())
+        historic = self.get_day_history(timestamp)
+        match_duration_total = 0
+        for match in historic:
+            url = f"{self.base_url}{match}?api_key={self.API_KEY}"
+            match_data = requests.get(url)
+            match_duration_total += match_data.json()['info']['gameDuration']
+        return match_duration_total / 3600
+
+class DiscordClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #self.debug = 0
+        self.triggered = False
+        self.humiliation_channel = None
+        self.play_time = float
+        self.snipper = lolSniper()
+        self.DISCORD_TOKEN = "MTA1NTU4NTA3ODc0ODY2MzgxOA.GbBDYa.dV9TqC_Ro4YBnHOo2wPBeGGTdDJ_ToetsVYDVM"
+
+    async def setup_hook(self) -> None:
+        self.my_background_task.start()
 
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-        self.triggered = False
-        self.humiliation_channel = client.get_guild(908448072730837139).get_channel(1055585865541353564)
-        self.match_played = 0
-        await self.my_loop()
-        return ""
-        
-    async def on_message(message):
-        print("new message")
-        
+        self.humiliation_channel = self.get_guild(908448072730837139).get_channel(1055585865541353564)
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
 
-API_KEY = "RGAPI-33ccdc25-6a4a-4e06-a61e-3002ce16ad96"
-SPOT_PUUID = "64c8Bnqtzo4JZX7OMpHKOCVoxlTi9KSwNEBxj9c710XnLDVPgYG4tw8ddb5AaEW_q9xOOgckB0Bjfg"
-DISCORD_TOKEN = "MTA1NTU4NTA3ODc0ODY2MzgxOA.GbBDYa.dV9TqC_Ro4YBnHOo2wPBeGGTdDJ_ToetsVYDVM"
+    @tasks.loop(seconds = 30)
+    async def my_background_task(self):
+        #self.debug = self.debug + 1
+        await loop(self)
+
+    @my_background_task.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()
+
+def get_embed_message(play_time :float):
+    embedVar = discord.Embed(title="Doc a encore fraudé", color=0xFF0000)
+    embedVar.add_field(name="Durée du préjudice ?", value=f"{timedelta(hours=play_time)}", inline=False)
+    embedVar.add_field(name="Piece a conviction", value="https://www.op.gg/summoners/euw/Larry444")
+    embedVar.set_footer(text="Made by 0xCurtis & Coach")
+    return embedVar
 
 def get_random_messages(play_time):
     time = str(timedelta(hours=play_time)).rsplit(':', 1)[0]
@@ -83,6 +79,25 @@ def get_random_messages(play_time):
     ]
     return array[random.randint(0, len(array) - 1)]
 
-intents = discord.Intents(value=8, guilds=True)
-client = MyClient(intents=intents)
-asyncio.run(client.run(DISCORD_TOKEN))
+async def set_presence(client :DiscordClient):
+    if client.play_time == 0:
+        await client.change_presence(activity=discord.Game(name=f"Doc n'a pas (encore) joué aujourd'hui. Bon toutou"))
+    else:
+        await client.change_presence(activity=discord.Game(name=f"Doc a joué {str(timedelta(hours=client.play_time)).rsplit(':', 1)[0]} aujourd'hui"))
+    
+async def post_message(client :DiscordClient):
+    if client.play_time > 3 and client.triggered == False:
+        await client.humiliation_channel.send(embed=get_embed_message(client.play_time))
+        client.triggered = True
+    elif client.play_time > 3 and client.triggered == True:
+        await client.humiliation_channel.send(str(get_random_messages(client.play_time)))
+
+async def loop (client :DiscordClient):
+    client.play_time = client.snipper.get_daily_play_time()
+    #client.play_time = client.debug
+    print(client.play_time)
+    await set_presence(client)
+    await post_message(client)
+
+client = DiscordClient(intents=discord.Intents.default())
+client.run(client.DISCORD_TOKEN)
